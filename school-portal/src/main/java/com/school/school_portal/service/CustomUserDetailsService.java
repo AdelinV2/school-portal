f6b2fdac8aae407e42ab30f6b8848fd9e5f45b91
Adelin <adelin10emil@gmail.com>
2024-12-09 16:18:05 +0200

    Added method findByEmail

---
+++ school-portal/src/main/java/com/school/school_portal/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.school.school_portal.service;
+
+import com.school.school_portal.entity.User;
+import com.school.school_portal.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository usersRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.usersRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(
+                "Could not found user"
+        ));
+
+        return new CustomUserDetails(user);
+    }
+}
