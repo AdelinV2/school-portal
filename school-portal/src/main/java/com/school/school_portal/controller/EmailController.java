c30c7124a4daf99a67db6fd968a37ebe1281afad
Adelin <adelin10emil@gmail.com>
2024-12-13 20:34:29 +0200

    Added method to email newly created accounts with their password

--- school-portal/src/main/java/com/school/school_portal/controller/EmailController.java
+++ school-portal/src/main/java/com/school/school_portal/controller/EmailController.java
@@ -1,9 +1,8 @@
 package com.school.school_portal.controller;
 
+import com.school.school_portal.entity.User;
 import com.school.school_portal.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +15,16 @@ public class EmailController {
         this.emailService = emailService;
     }
 
-/*
-    @RequestMapping("/")
-    public String sendEmail(){
-        emailService.sendEmail(emailUsername, "Test", "Test");
-        return "Email sent";
-    }
+    public void sendEmailNewAccount(User user, String password) {
 
- */
+        String emailSubject = "Account Created";
+
+        String emailBody = "Hello " + user.getRole().getName() + " " + user.getFullName() + ",\n\n" +
+                "Your school account has been created. Your password is: " + password + "\n\n" +
+                "Please login to the system and change your password.\n\n" +
+                "Regards,\n" +
+                "Spring School Office";
+
+        emailService.sendEmail(user.getEmail(), emailSubject, emailBody);
+    }
 }
