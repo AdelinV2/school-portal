127c59a5ab7e7e4fafded7e106fdfc8766c1315e
Adelin <adelin10emil@gmail.com>
2024-12-13 15:50:28 +0200

    Added method to find by user full name

--- school-portal/src/main/java/com/school/school_portal/repository/TeacherRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/TeacherRepository.java
@@ -3,5 +3,9 @@ package com.school.school_portal.repository;
 import com.school.school_portal.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    
+    Optional<Teacher> findByUser_FullName(String tutor);
 }
