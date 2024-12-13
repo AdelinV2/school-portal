db24e85ffb21e333d2510f099df5642595e21f5f
Adelin <adelin10emil@gmail.com>
2024-12-13 15:29:31 +0200

    Added ClassController

---
+++ school-portal/src/main/java/com/school/school_portal/controller/ClassController.java
@@ -0,0 +1,50 @@
+package com.school.school_portal.controller;
+
+import com.school.school_portal.dto.ClassForm;
+import com.school.school_portal.service.ClassService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class ClassController {
+
+    private final ClassService classService;
+
+    @Autowired
+    public ClassController(ClassService classService) {
+        this.classService = classService;
+    }
+
+    @GetMapping("/admin/add-class")
+    public String showAddClassForm(Model model) {
+
+        model.addAttribute("classForm", new ClassForm());
+        model.addAttribute("classes", classService.getAllClasses());
+        model.addAttribute("years", classService.getAllYears());
+        model.addAttribute("sections", classService.getAllSections());
+        model.addAttribute("tutors", classService.getAllTutors());
+
+        return "add/add-class";
+    }
+
+    @PostMapping("/admin/add-class")
+    public String addClass(@Valid ClassForm classForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("classForm", classForm);
+            model.addAttribute("classes", classService.getAllClasses());
+            model.addAttribute("years", classService.getAllYears());
+            model.addAttribute("sections", classService.getAllSections());
+            model.addAttribute("tutors", classService.getAllTutors());
+            return "add/add-class";
+        }
+
+        return "redirect:/admin/classes";
+    }
+}
