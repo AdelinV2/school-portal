b9b239eed4ddfa8f80281f3722644ecb7960e560
Adelin <adelin10emil@gmail.com>
2024-12-14 13:22:18 +0200

    Added method to find by teacher's email

--- school-portal/src/main/java/com/school/school_portal/repository/ClassRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/ClassRepository.java
@@ -2,6 +2,13 @@ package com.school.school_portal.repository;
 
 import com.school.school_portal.entity.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClassRepository extends JpaRepository<Class, Integer> {
+
+    @Query("SELECT DISTINCT c.classField FROM ClassCourse c WHERE c.course.teacher.user.email = :email")
+    List<Class> findByTeacherUserEmail(@Param("email") String email);
 }
