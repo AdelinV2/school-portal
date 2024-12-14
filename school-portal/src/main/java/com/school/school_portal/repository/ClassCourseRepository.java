67166906c66f0e887ff4b7f4e1241f408777904b
Adelin <adelin10emil@gmail.com>
2024-12-14 16:45:49 +0200

    Added method to find courses in a class by class id

--- school-portal/src/main/java/com/school/school_portal/repository/ClassCourseRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/ClassCourseRepository.java
@@ -3,5 +3,9 @@ package com.school.school_portal.repository;
 import com.school.school_portal.entity.ClassCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClassCourseRepository extends JpaRepository<ClassCourse, Integer> {
+
+    List<ClassCourse> findByClassId(Integer classId);
 }
