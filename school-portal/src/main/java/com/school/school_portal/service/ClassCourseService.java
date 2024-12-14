966eae08ec4b0ca19c46fdc58ea236614effa4b1
Adelin <adelin10emil@gmail.com>
2024-12-14 16:53:23 +0200

    Added method to get all courses in a class by class id

--- school-portal/src/main/java/com/school/school_portal/service/ClassCourseService.java
+++ school-portal/src/main/java/com/school/school_portal/service/ClassCourseService.java
@@ -1,8 +1,11 @@
 package com.school.school_portal.service;
 
+import com.school.school_portal.entity.ClassCourse;
 import com.school.school_portal.repository.ClassCourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClassCourseService {
@@ -14,5 +17,7 @@ public class ClassCourseService {
         this.classCourseRepository = classCourseRepository;
     }
 
-
+    public List<ClassCourse> getClassCoursesByClassId(Integer classId) {
+        return classCourseRepository.findByClassFieldId(classId);
+    }
 }
