0680a08b636e67dc795c2bca89199477e68ddc59
Adelin <adelin10emil@gmail.com>
2024-12-14 13:24:09 +0200

    Added method to get the number of students from a class

--- school-portal/src/main/java/com/school/school_portal/repository/StudentRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/StudentRepository.java
@@ -4,4 +4,6 @@ import com.school.school_portal.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    int countByClassField_Id(Integer classId);
 }
