39aa4d9e8598aa2475447dca08d2438fec29d9e1
Adelin <adelin10emil@gmail.com>
2024-12-14 12:40:37 +0200

    Deleted private final StudentService

--- school-portal/src/main/java/com/school/school_portal/service/StudentService.java
+++ school-portal/src/main/java/com/school/school_portal/service/StudentService.java
@@ -8,13 +8,11 @@ import org.springframework.stereotype.Service;
 public class StudentService {
 
     private final StudentRepository studentRepository;
-    private final StudentService studentService;
     private final UserService userService;
 
     @Autowired
     public StudentService(StudentRepository studentRepository, StudentService studentService, UserService userService) {
         this.studentRepository = studentRepository;
-        this.studentService = studentService;
         this.userService = userService;
     }
 }
