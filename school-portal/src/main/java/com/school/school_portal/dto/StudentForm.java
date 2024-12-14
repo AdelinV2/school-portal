3052616a1aa58916765c29e3ee33622cf13a13c1
Adelin <adelin10emil@gmail.com>
2024-12-14 12:14:15 +0200

    Added StudentForm

---
+++ school-portal/src/main/java/com/school/school_portal/dto/StudentForm.java
@@ -0,0 +1,39 @@
+package com.school.school_portal.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class StudentForm {
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
