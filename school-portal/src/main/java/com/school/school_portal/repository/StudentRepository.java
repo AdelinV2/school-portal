fe7978f6258aa790924315631d045089bb6ab92c
Adelin <adelin10emil@gmail.com>
2024-12-15 13:04:48 +0200

    Added method to get all students within a class by class id

--- school-portal/src/main/java/com/school/school_portal/repository/StudentRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/StudentRepository.java
@@ -3,7 +3,11 @@ package com.school.school_portal.repository;
 import com.school.school_portal.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     int countByClassField_Id(Integer classId);
+
+    List<Student> findAllByClassField_Id(Integer classId);
 }
