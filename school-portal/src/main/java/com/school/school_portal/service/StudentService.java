58b02da2ce270235e3c4cc9cf17d284000ba5c9e
Adelin <adelin10emil@gmail.com>
2024-12-15 13:49:41 +0200

    Added method to save a student

--- school-portal/src/main/java/com/school/school_portal/service/StudentService.java
+++ school-portal/src/main/java/com/school/school_portal/service/StudentService.java
@@ -1,6 +1,9 @@
 package com.school.school_portal.service;
 
+import com.school.school_portal.dto.StudentForm;
+import com.school.school_portal.entity.Role;
 import com.school.school_portal.entity.Student;
+import com.school.school_portal.entity.User;
 import com.school.school_portal.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +15,39 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
     private final UserService userService;
+    private final ClassService classService;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository, UserService userService) {
+    public StudentService(StudentRepository studentRepository, UserService userService, ClassService classService) {
         this.studentRepository = studentRepository;
         this.userService = userService;
+        this.classService = classService;
     }
 
     public List<Student> getStudentsByClassId(Integer classId) {
         return studentRepository.findAllByClassField_Id(classId);
+    }
+
+    public boolean saveStudent(StudentForm studentForm, Integer classId) {
+
+        User newUser = new User();
+        Student newStudent = new Student();
+
+        newUser.setFirstName(studentForm.getFirstName());
+        newUser.setLastName(studentForm.getLastName());
+        newUser.setEmail(studentForm.getEmail());
+        newUser.setActive(false);
+        newUser.setRole(new Role(3, "Student"));
+
+        if(!userService.saveNewUserWithRandomPassword(newUser)) {
+            return false;
+        }
+
+        newStudent.setUser(newUser);
+        newStudent.setClassField(classService.getClassById(classId));
+
+        studentRepository.save(newStudent);
+
+        return true;
     }
 }
