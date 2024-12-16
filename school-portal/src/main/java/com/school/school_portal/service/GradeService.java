7332ac3fb250bb3ac6a9a8bf12703fecda2d77d6
Adelin <adelin10emil@gmail.com>
2024-12-16 10:56:15 +0200

    Added methods to get average grade for a course and total average grade for a student

--- school-portal/src/main/java/com/school/school_portal/service/GradeService.java
+++ school-portal/src/main/java/com/school/school_portal/service/GradeService.java
@@ -1,12 +1,13 @@
 package com.school.school_portal.service;
 
 import com.school.school_portal.dto.GradeForm;
-import com.school.school_portal.entity.Course;
 import com.school.school_portal.entity.Grade;
 import com.school.school_portal.repository.GradeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Service
@@ -37,5 +38,30 @@ public class GradeService {
 
     public List<Grade> getGradesByStudentIdAndClassCourseId(Integer studentId, Integer classCourseId) {
         return gradeRepository.findAllByStudent_IdAndClassCourse_Id(studentId, classCourseId);
+    }
+
+    public BigDecimal getAverageGradeByStudentIdAndClassCourseId(Integer studentId, Integer classCourseId) {
+
+        List<Grade> grades = getGradesByStudentIdAndClassCourseId(studentId, classCourseId);
+        BigDecimal avg = new BigDecimal(0);
+
+        for (Grade grade : grades) {
+            avg = avg.add(grade.getGrade());
+        }
+
+        return avg.divide(new BigDecimal(grades.size()), 2, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal getTotalAverageGradeByStudentId(Integer studentId) {
+
+        Integer classId = studentService.getStudentById(studentId).getClassField().getId();
+        List<Integer> courseIds = classCourseService.getClassCourseIdsByClassId(classId);
+        BigDecimal avg = new BigDecimal(0);
+
+        for (Integer courseId : courseIds) {
+            avg = avg.add(getAverageGradeByStudentIdAndClassCourseId(studentId, courseId));
+        }
+
+        return avg.divide(new BigDecimal(courseIds.size()), 2, RoundingMode.HALF_UP);
     }
 }
