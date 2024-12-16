7dd556085603c2d228d76d701e9c79540e6b7da8
Adelin <adelin10emil@gmail.com>
2024-12-16 10:35:28 +0200

    Added method to save a grade

--- school-portal/src/main/java/com/school/school_portal/service/GradeService.java
+++ school-portal/src/main/java/com/school/school_portal/service/GradeService.java
@@ -1,5 +1,9 @@
 package com.school.school_portal.service;
 
+import com.school.school_portal.dto.GradeForm;
+import com.school.school_portal.entity.Course;
+import com.school.school_portal.entity.Grade;
+import com.school.school_portal.repository.GradeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,9 +11,25 @@ import org.springframework.stereotype.Service;
 public class GradeService {
 
     private final StudentService studentService;
+    private final GradeRepository gradeRepository;
+    private final ClassCourseService classCourseService;
 
     @Autowired
-    public GradeService(StudentService studentService) {
+    public GradeService(StudentService studentService, GradeRepository gradeRepository, ClassCourseService classCourseService) {
         this.studentService = studentService;
+        this.gradeRepository = gradeRepository;
+        this.classCourseService = classCourseService;
+    }
+
+    public void saveGrade(GradeForm gradeForm) {
+
+        Grade newGrade = new Grade();
+
+        newGrade.setStudent(studentService.getStudentById(gradeForm.getStudentId()));
+        newGrade.setClassCourse(classCourseService.getClassCourseByCourseId(gradeForm.getCourseId()));
+        newGrade.setGrade(gradeForm.getGrade());
+        newGrade.setDateAssigned(gradeForm.getDateAssigned());
+
+        gradeRepository.save(newGrade);
     }
 }
