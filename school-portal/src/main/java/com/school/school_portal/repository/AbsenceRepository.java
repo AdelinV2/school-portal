6d8e4b2b5ab22f0e0c218f3f678fd95a5da90d3c
Adelin <adelin10emil@gmail.com>
2024-12-16 12:51:28 +0200

    Added method to find absences of a student

--- school-portal/src/main/java/com/school/school_portal/repository/AbsenceRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/AbsenceRepository.java
@@ -3,5 +3,9 @@ package com.school.school_portal.repository;
 import com.school.school_portal.entity.Absence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AbsenceRepository extends JpaRepository<Absence, Integer> {
+
+    List<Absence> findByStudent_IdAndExcused(Integer studentId, boolean excused);
 }
