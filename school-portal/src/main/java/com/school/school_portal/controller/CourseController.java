e5ea78fafb5ebf040584862314ca16c93c153d43
Adelin <adelin10emil@gmail.com>
2024-12-18 13:59:24 +0200

    Added mappings for editing a course and added teacher service

--- school-portal/src/main/java/com/school/school_portal/controller/CourseController.java
+++ school-portal/src/main/java/com/school/school_portal/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.school.school_portal.controller;
 import com.school.school_portal.dto.CourseForm;
 import com.school.school_portal.service.ClassService;
 import com.school.school_portal.service.CourseService;
+import com.school.school_portal.service.TeacherService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,11 +18,13 @@ public class CourseController {
 
     private final CourseService courseService;
     private final ClassService classService;
+    private final TeacherService teacherService;
 
     @Autowired
-    public CourseController(CourseService courseService, ClassService classService) {
+    public CourseController(CourseService courseService, ClassService classService, TeacherService teacherService) {
         this.courseService = courseService;
         this.classService = classService;
+        this.teacherService = teacherService;
     }
 
     @GetMapping("/admin/add-course/{id}")
@@ -29,7 +32,7 @@ public class CourseController {
 
         model.addAttribute("courseForm", new CourseForm());
         model.addAttribute("subjects", courseService.getAllCoursesSubjects());
-        model.addAttribute("teachers", courseService.getAllTeachers());
+        model.addAttribute("teachers", teacherService.getAllTeachers());
         model.addAttribute("class", classService.getClassById(id));
 
         return "add/add-course";
@@ -42,12 +45,37 @@ public class CourseController {
 
             model.addAttribute("courseForm", courseForm);
             model.addAttribute("subjects", courseService.getAllCoursesSubjects());
-            model.addAttribute("teachers", courseService.getAllTeachers());
+            model.addAttribute("teachers", teacherService.getAllTeachers());
 
             return "add/add-course/{id}";
         }
 
         courseService.saveCourse(courseForm, id);
+
+        return "redirect:/admin/class/{id}";
+    }
+
+    @GetMapping("/admin/edit-course/{id}")
+    public String showEditCourseForm(@PathVariable Integer id, CourseForm courseForm, Model model) {
+
+        model.addAttribute("courseForm", courseForm);
+        model.addAttribute("teachers", teacherService.getAllTeachers());
+
+        return "update/update-course";
+    }
+
+    @PostMapping("/admin/edit-course/{id}")
+    public String editCourse(@PathVariable Integer id, @Valid CourseForm courseForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("courseForm", courseForm);
+            model.addAttribute("teachers", teacherService.getAllTeachers());
+
+            return "update/update-course";
+        }
+
+        courseService.updateCourse(courseForm, id);
 
         return "redirect:/admin/class/{id}";
     }
