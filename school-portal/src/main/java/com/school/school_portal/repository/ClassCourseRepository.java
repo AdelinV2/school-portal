c82fc7cf70b6f745418c875778d42a92793910ef
Adelin <adelin10emil@gmail.com>
2025-01-03 15:05:49 +0200

    Fixed method names

--- school-portal/src/main/java/com/school/school_portal/repository/ClassCourseRepository.java
+++ school-portal/src/main/java/com/school/school_portal/repository/ClassCourseRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface ClassCourseRepository extends JpaRepository<ClassCourse, Integer> {
 
-    List<ClassCourse> findByClassFieldId(Integer classId);
+    List<ClassCourse> findByClassField_Id(Integer classId);
 
-    ClassCourse findByCourseId(Integer courseId);
+    ClassCourse findByCourse_Id(Integer courseId);
 
     @Query("SELECT c.course.id FROM ClassCourse c WHERE c.classField.id = :classFieldId")
     List<Integer> findCourseIdsByClassField_Id(@Param("classFieldId") Integer classFieldId);
